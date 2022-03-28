f50e35eb37b3f736a7a6c10832b386583a3f6e69
Dipen prajapti <DP183@myscc.ca>
2022-03-28 01:12:11 -0400

    Category Database updated

--- DipensBooks.DataAccess/Data/ApplicationDbContext.cs
+++ DipensBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DipensBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,6 @@ namespace DipensBookStore.DataAccess.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
--- DipensBooks.Models/Category.cs
+++ DipensBooks.Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DipensBooks.Models
@@ -7,5 +8,13 @@ namespace DipensBooks.Models
     public class Category
     {
         [Key]
+        public int Id { get; set; }
+        
+        [Display(Name="Category Name")]
+        [Required]
+        [MaxLength(50)]
+
+        public string Name { get; set; }
+
     }
 }
